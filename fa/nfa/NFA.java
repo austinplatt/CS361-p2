90863d6346388dbb28dea80c28e99661e631fd10
austinplatt <austinplatt@u.boisestate.edu>
2023-10-23 20:48:09 -0600

    "Updates to maxCopies and accepts methods"

--- fa/nfa/NFA.java
+++ fa/nfa/NFA.java
@@ -92,12 +92,12 @@ public class NFA implements NFAInterface {
 
 				Set<NFAState> snapshot = new HashSet<NFAState>();
 				snapshot.addAll(currentStates);
+				newStates.clear();
 				while (!currentStates.isEmpty()) {
 					Set<NFAState> eClose = eClosure(currentStates.peek());
 					eClose.removeAll(snapshot);
 					currentStates.addAll(eClose);
 					snapshot.addAll(currentStates);
-					newStates.clear();
 					newStates.addAll(getToState(currentStates.remove(), symbol));
 				}
 				currentStates.addAll(newStates);
@@ -153,52 +153,54 @@ public class NFA implements NFAInterface {
 		return startState.getName().equals(name);
 	}
 
-    @Override
-    public int maxCopies(String s) {
-        Queue<NFAState> currentStates = new LinkedList<NFAState>();
-        currentStates.add(startState);
-        int retval = 0;
-    
-        Queue<NFAState> newStates = new LinkedList<NFAState>();
-    
+	@Override
+	public int maxCopies(String s) {
+		Queue<NFAState> currentStates = new LinkedList<NFAState>();
+		currentStates.add(startState);
+		int retval = 1;
+		
+		Queue<NFAState> newStates = new LinkedList<NFAState>();
         if (s != "e") {
-            for (char symbol : s.toCharArray()) {
-                if (!alphabet.contains(symbol)) {
-                    return retval;
-                }
-    
-                // Create a snapshot of the current states
-                Set<NFAState> snapshot = new HashSet<>(currentStates);
-    
-                while (!currentStates.isEmpty()) {
-                    Set<NFAState> eClose = eClosure(currentStates.peek());
-                    eClose.removeAll(snapshot);
-                    currentStates.addAll(eClose);
-                    snapshot.addAll(currentStates);
-                    newStates.clear();
-                    newStates.addAll(getToState(currentStates.remove(), symbol));
-                }
-    
-                currentStates.addAll(newStates);
-            }
+			for (char symbol : s.toCharArray()) {
+//				if (!alphabet.contains(symbol)) {
+//					return retval;
+//				}
+
+				Set<NFAState> snapshot = new HashSet<NFAState>();
+				snapshot.addAll(currentStates);
+				newStates.clear();
+				while (!currentStates.isEmpty()) {
+					Set<NFAState> eClose = eClosure(currentStates.peek());
+					eClose.removeAll(snapshot);
+					currentStates.addAll(eClose);
+					snapshot.addAll(currentStates);
+					retval = Math.max(retval, currentStates.size());
+					if(alphabet.contains(symbol)) {
+						newStates.addAll(getToState(currentStates.remove(), symbol));
+					} else {
+						currentStates.remove();
+					}
+				}
+				currentStates.addAll(newStates);
+			}
         }
-    
-        for (NFAState state : currentStates) {
-            Set<NFAState> eClose = eClosure(state);
-            eClose.removeAll(newStates);
-            newStates.addAll(eClose);
-        }
-    
-        newStates.removeAll(currentStates);
+			for (NFAState state : currentStates) {
+				Set<NFAState> eClose = eClosure(state);
+				eClose.removeAll(newStates);
+				newStates.addAll(eClose);
+
+			} 
+		newStates.removeAll(currentStates);
         currentStates.addAll(newStates);
-    
-        // Calculate the maximum number of copies
-        for (NFAState state : currentStates) {
-            retval = Math.max(retval, eClosure(state).size());
-        }
-    
+        retval = Math.max(retval, currentStates.size());
+        
+        
+//        for (NFAState finalState : finalStates) {
+//        	if (currentStates.contains(finalState)) retval = 0;
+//        }
+
         return retval;
-    }    
+	}
 
 	@Override
 	public boolean addTransition(String fromState, Set<String> toStates, char onSymb) {
