b0a944068ba5a1544eaba038d2a4166829116552
austinplatt <austinplatt@u.boisestate.edu>
2023-10-23 19:58:58 -0600

    "Updates to NFA"

--- fa/nfa/NFA.java
+++ fa/nfa/NFA.java
@@ -154,45 +154,37 @@ public boolean accepts(String s) {
 		return startState.getName().equals(name);
 	}
 
-	@Override
-	public int maxCopies(String s) {
-		Queue<NFAState> currentStates = new LinkedList<NFAState>();
-		currentStates.add(startState);
-		int retval = 0;
-		
-		Queue<NFAState> newStates = new LinkedList<NFAState>();
-        if (s != "e") {
-			for (char symbol : s.toCharArray()) {
-				if (!alphabet.contains(symbol)) {
-					return 1;
-				}
-
-				while (!currentStates.isEmpty()) {
-					Set<NFAState> eClose = eClosure(currentStates.peek());
-					eClose.remove(currentStates.peek());
-					currentStates.addAll(eClose);
-					retval = Math.max(retval, currentStates.size());
-					newStates.addAll(getToState(currentStates.remove(), symbol));
-				}
-				currentStates.addAll(newStates);
-			}
-        }
-		for (NFAState state : currentStates) {
-			Set<NFAState> eClose = eClosure(state);
-			eClose.removeAll(newStates);
-			newStates.addAll(eClose);
-			} 
-		    newStates.removeAll(currentStates);
-            currentStates.addAll(newStates);
+    @Override
+    public int maxCopies(String s) {
+        Queue<NFAState> currentStates = new LinkedList<NFAState>();
+        currentStates.add(startState);
+        int retval = 0;
+    
+        Queue<NFAState> newStates = new LinkedList<NFAState>();
+    
+        if (!s.equals("e")) {
+            for (char symbol : s.toCharArray()) {
+                if (!alphabet.contains(symbol)) {
+                    return 1;
+                }
+    
+                while (!currentStates.isEmpty()) {
+                    Set<NFAState> eClose = eClosure(currentStates.peek());
+                    eClose.remove(currentStates.peek());
+                    currentStates.addAll(eClose);
+                    newStates.addAll(getToState(currentStates.remove(), symbol));
+                }
+                currentStates.addAll(newStates);
+            }
+    
             retval = Math.max(retval, currentStates.size());
-        
-        
-//        for (NFAState finalState : finalStates) {
-//        	if (currentStates.contains(finalState)) retval = 0;
-//        }
-
+        } else {
+            retval = 1; // Handle epsilon transition
+        }
+    
         return retval;
-	}
+    }
+    
 
 	@Override
 	public boolean addTransition(String fromState, Set<String> toStates, char onSymb) {
